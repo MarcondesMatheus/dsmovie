e026a50c01255b5de73c7ac118a4809924f0e6d3
MarcondesMatheus <matheustkp1@gmail.com>
2022-03-18 17:16:31 -0300

    navbarnick

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/MarcondesMatheus">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
-                            <p className='dsmovie-contact-link'>/devsuperior</p>
+                            <p className='dsmovie-contact-link'>/MarcondesMatheus</p>
                         </div>
                     </a>
                 </div>
